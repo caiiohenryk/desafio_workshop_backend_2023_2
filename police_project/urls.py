2f0346b678ecb77b1736df6e1865f48f77a58ddb
Caio Henrique <129773231+caiiohenryk@users.noreply.github.com>
2023-09-02 23:25:47 -0300

    Delete police_project directory

--- police_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-# Incluindo as url's do app no projeto.
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('police_app.urls')),
-]
