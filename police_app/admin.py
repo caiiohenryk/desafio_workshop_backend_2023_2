99e3e3dd2c749f87cef9abf47e576e6b64e3f1ae
Caio Henrique <129773231+caiiohenryk@users.noreply.github.com>
2023-09-02 23:25:34 -0300

    Delete police_app directory

--- police_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Pessoa, Ocorrencia
-
-# Registrando as tables no /admin
-
-admin.site.register(Pessoa)
-admin.site.register(Ocorrencia)
--- police_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-# Configura o app
-
-class PoliceAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'police_app'
